bf7a046fe31f92d4d3bc1a693c8ae10b3cbdd9a5
ujjessha <64350773+ujjessha@users.noreply.github.com>
2020-07-02 15:29:05 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,36 +1,41 @@
 class Dustbin{
     constructor(x,y,width,height){
+        this.dustbinWidth=200;
+        this.dustbinHeight=200;
+	    this.wallThickness=20;
+        this.angle=0;	
+		
+      
 
-        this.image=loadImage("dustbingreen.png");
-        
-        var options={
-            isStatic:false,
-            restituution:0.3,
-            friction:0.5,
-            density:1.2
-
-        }
+    this.bottomBody=Bodies.rectangle(x, y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+    this.leftWallBody=Bodies.rectangle(x-this.dustbinWidth/2, y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+    Matter.Body.setAngle(this.leftWallBody, this.angle);
     
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
+    this.image=loadImage("dustbingreen.png")
+
+   this.rightWallBody=Bodies.rectangle(x+this.dustbinWidth/2, y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+   Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
+   World.add(world, this.bottomBody)
+   World.add(world, this.leftWallBody)
+   World.add(world, this.rightWallBody);
+   }
+
+ display(){ 
+	 
+    var posBottom=this.bottomBody.position;
+    var posLeft=this.leftWallBody.position;
+    var posRight=this.rightWallBody.position;
+
     
-    World.add(world, this.body);
-    }
 
- display(){
 
-    var pos =this.body.position;
-   
-    push();
-
-    translate(pos.x,pos.y);
-    rectMode(CENTER);
-    console.log(this.width);
-    console.log(this.height);
-
-    rect(0, 0, this.width, this.height);
-    image(this.image,0 ,0 , 150, 20);
-    pop();
+  push()
+  translate(posBottom.x, posBottom.y-180);
+  rectMode(CENTER)
+  angleMode(RADIANS)
+  fill(255)
+  imageMode(CENTER);
+  image(this.image, 0,this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+  pop()
   }
 }
